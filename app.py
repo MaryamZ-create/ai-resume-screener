79a2351b1466a2a8165c6c5d7a727fb0bbe88bab
MaryamZ-create <maryamzahoor1221@gmail.com>
2026-07-24 13:18:55 +0000

    Add configurable backend URL

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import requests
+import os
 
-BACKEND_URL = "http://localhost:8000"
+
+BACKEND_URL = os.getenv(
+    "BACKEND_URL",
+    "http://localhost:8000"
+)
 
 st.set_page_config(
     page_title="AI Resume Screener",
